49c5b662c6a917e59cfd5226a678ddf2a73b2980
shubiandeluoye <wangjian85962674@163.com>
2024-12-31 02:45:58 +0800

    查找问题

--- Assets/Scripts/Input/WJPlayerControls.cs
+++ Assets/Scripts/Input/WJPlayerControls.cs
@@ -1,4 +1,6 @@
+
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
@@ -17,122 +19,122 @@ public class WJPlayerControls : IInputActionCollection2, IDisposable
     public WJPlayerControls()
     {
         asset = InputActionAsset.FromJson(@"{
-    ""name"": ""PlayerControls"",
-    ""maps"": [
-        {
-            ""name"": ""Player"",
-            ""id"": ""f62a4b92-ef5e-4175-8f4c-c9075429d32c"",
-            ""actions"": [
+            ""name"": ""PlayerControls"",
+            ""maps"": [
                 {
-                    ""name"": ""Move"",
-                    ""type"": ""Value"",
-                    ""id"": ""6bc1aaf4-b110-4ff7-891e-5b9fe6f32c4d"",
-                    ""expectedControlType"": ""Vector2"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": true
+                    ""name"": ""Player"",
+                    ""id"": ""f62a4b92-ef5e-4175-8f4c-c9075429d32c"",
+                    ""actions"": [
+                        {
+                            ""name"": ""Move"",
+                            ""type"": ""Value"",
+                            ""id"": ""6bc1aaf4-b110-4ff7-891e-5b9fe6f32c4d"",
+                            ""expectedControlType"": ""Vector2"",
+                            ""processors"": """",
+                            ""interactions"": """",
+                            ""initialStateCheck"": true
+                        }
+                    ],
+                    ""bindings"": [
+                        {
+                            ""name"": ""WASD"",
+                            ""id"": ""00ca640b-d935-4593-8157-c05846ea39b3"",
+                            ""path"": ""Dpad"",
+                            ""interactions"": """",
+                            ""processors"": """",
+                            ""groups"": """",
+                            ""action"": ""Move"",
+                            ""isComposite"": true,
+                            ""isPartOfComposite"": false
+                        },
+                        {
+                            ""name"": ""up"",
+                            ""id"": ""e2062cb9-1b15-46a2-838c-2f8d72a0bdd9"",
+                            ""path"": ""<Keyboard>/w"",
+                            ""interactions"": """",
+                            ""processors"": """",
+                            ""groups"": "";Keyboard&Mouse"",
+                            ""action"": ""Move"",
+                            ""isComposite"": false,
+                            ""isPartOfComposite"": true
+                        },
+                        {
+                            ""name"": ""down"",
+                            ""id"": ""320bffee-a40b-4347-ac70-c210eb8bc73a"",
+                            ""path"": ""<Keyboard>/s"",
+                            ""interactions"": """",
+                            ""processors"": """",
+                            ""groups"": "";Keyboard&Mouse"",
+                            ""action"": ""Move"",
+                            ""isComposite"": false,
+                            ""isPartOfComposite"": true
+                        },
+                        {
+                            ""name"": ""left"",
+                            ""id"": ""d2581a9b-1d11-4566-b27d-b92aff5fabbc"",
+                            ""path"": ""<Keyboard>/a"",
+                            ""interactions"": """",
+                            ""processors"": """",
+                            ""groups"": "";Keyboard&Mouse"",
+                            ""action"": ""Move"",
+                            ""isComposite"": false,
+                            ""isPartOfComposite"": true
+                        },
+                        {
+                            ""name"": ""right"",
+                            ""id"": ""fcfe95b8-67b9-4526-84b5-5d0bc98d6400"",
+                            ""path"": ""<Keyboard>/d"",
+                            ""interactions"": """",
+                            ""processors"": """",
+                            ""groups"": "";Keyboard&Mouse"",
+                            ""action"": ""Move"",
+                            ""isComposite"": false,
+                            ""isPartOfComposite"": true
+                        },
+                        {
+                            ""name"": """",
+                            ""id"": ""05f6913d-c316-48b2-a6bb-e225f14c7960"",
+                            ""path"": ""<Touchscreen>/primaryTouch/delta"",
+                            ""interactions"": """",
+                            ""processors"": """",
+                            ""groups"": "";Touch"",
+                            ""action"": ""Move"",
+                            ""isComposite"": false,
+                            ""isPartOfComposite"": false
+                        }
+                    ]
                 }
             ],
-            ""bindings"": [
+            ""controlSchemes"": [
                 {
-                    ""name"": ""WASD"",
-                    ""id"": ""00ca640b-d935-4593-8157-c05846ea39b3"",
-                    ""path"": ""Dpad"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": true,
-                    ""isPartOfComposite"": false
+                    ""name"": ""Keyboard&Mouse"",
+                    ""bindingGroup"": ""Keyboard&Mouse"",
+                    ""devices"": [
+                        {
+                            ""devicePath"": ""<Keyboard>"",
+                            ""isOptional"": false,
+                            ""isOR"": false
+                        },
+                        {
+                            ""devicePath"": ""<Mouse>"",
+                            ""isOptional"": false,
+                            ""isOR"": false
+                        }
+                    ]
                 },
                 {
-                    ""name"": ""up"",
-                    ""id"": ""e2062cb9-1b15-46a2-838c-2f8d72a0bdd9"",
-                    ""path"": ""<Keyboard>/w"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""320bffee-a40b-4347-ac70-c210eb8bc73a"",
-                    ""path"": ""<Keyboard>/s"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""left"",
-                    ""id"": ""d2581a9b-1d11-4566-b27d-b92aff5fabbc"",
-                    ""path"": ""<Keyboard>/a"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""right"",
-                    ""id"": ""fcfe95b8-67b9-4526-84b5-5d0bc98d6400"",
-                    ""path"": ""<Keyboard>/d"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""05f6913d-c316-48b2-a6bb-e225f14c7960"",
-                    ""path"": ""<Touchscreen>/primaryTouch/delta"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Touch"",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
+                    ""name"": ""Touch"",
+                    ""bindingGroup"": ""Touch"",
+                    ""devices"": [
+                        {
+                            ""devicePath"": ""<Touchscreen>"",
+                            ""isOptional"": false,
+                            ""isOR"": false
+                        }
+                    ]
                 }
             ]
-        }
-    ],
-    ""controlSchemes"": [
-        {
-            ""name"": ""Keyboard&Mouse"",
-            ""bindingGroup"": ""Keyboard&Mouse"",
-            ""devices"": [
-                {
-                    ""devicePath"": ""<Keyboard>"",
-                    ""isOptional"": false,
-                    ""isOR"": false
-                },
-                {
-                    ""devicePath"": ""<Mouse>"",
-                    ""isOptional"": false,
-                    ""isOR"": false
-                }
-            ]
-        },
-        {
-            ""name"": ""Touch"",
-            ""bindingGroup"": ""Touch"",
-            ""devices"": [
-                {
-                    ""devicePath"": ""<Touchscreen>"",
-                    ""isOptional"": false,
-                    ""isOR"": false
-                }
-            ]
-        }
-    ]
-}");
+        }");
         // Player
         m_Player_Move = asset.FindAction("Player/Move", throwIfNotFound: true);
     }
@@ -152,6 +154,8 @@ public class WJPlayerControls : IInputActionCollection2, IDisposable
         set => asset.bindingMask = value;
     }
 
+    public IEnumerable<InputBinding> bindings => asset.bindings;
+
     public ReadOnlyArray<InputDevice>? devices
     {
         get => asset.devices;
@@ -161,18 +165,6 @@ public class WJPlayerControls : IInputActionCollection2, IDisposable
     public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
 
     public bool Contains(InputAction action) => asset.Contains(action);
-
-    public InputAction FindAction(string actionNameOrId, bool throwIfNotFound = false)
-    {
-        return asset.FindAction(actionNameOrId, throwIfNotFound);
-    }
-
-    public bool FindBinding(InputBinding bindingMask, out InputAction action)
-    {
-        return asset.FindBinding(bindingMask, out action);
-    }
-
-    public ReadOnlyArray<InputBinding> bindings => asset.bindings;
 
     public IEnumerator<InputAction> GetEnumerator() => asset.GetEnumerator();
 
@@ -187,7 +179,6 @@ public class WJPlayerControls : IInputActionCollection2, IDisposable
     public interface IPlayerActions
     {
         void OnMove(InputAction.CallbackContext context);
-        InputAction Move { get; }
     }
 
     private class PlayerActions : IPlayerActions
@@ -204,5 +195,16 @@ public class WJPlayerControls : IInputActionCollection2, IDisposable
         }
 
         public InputAction Move => m_Wrapper.m_Player_Move;
+    }
+
+    // 实现 IInputActionCollection2 接口成员
+    public int FindBinding(InputBinding mask, out InputAction action)
+    {
+        return asset.FindBinding(mask, out action);
+    }
+
+    public InputAction FindAction(string actionNameOrId, bool throwIfNotFound = false)
+    {
+        return asset.FindAction(actionNameOrId, throwIfNotFound);
     }
 }
